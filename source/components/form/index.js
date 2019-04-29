d1c409c8a493b8e26ac9187af8ec92e17b4d410e
zavoluk <zavoluk@gmail.com>
2019-04-29 23:32:36 +0300

    new layout fix

--- source/components/form/index.js
+++ source/components/form/index.js
@@ -1,1 +1,2 @@
 import './index.sass'
+import '../../js/input-mask'
---
+++ source/js/input-mask.js
@@ -0,0 +1,29 @@
+import IMask from 'imask'
+
+let telInputs = document.querySelectorAll('.input_tel'),
+	nameInputs = document.querySelectorAll('.input_name')
+
+for (let telInput of telInputs) {
+	let patternMask = new IMask(telInput, {
+		mask: '+{7}(000)000-00-00',
+		lazy: true
+	})
+
+	telInput.addEventListener('focus', function () {
+		patternMask.updateOptions({lazy: false})
+	}, true);
+
+	telInput.addEventListener('blur', function () {
+		patternMask.updateOptions({lazy: false})
+		if (!patternMask.masked.rawInputValue.length) {
+			patternMask.updateOptions({lazy: true})
+			patternMask.value = ''
+		}
+	}, true)
+}
+
+for (let nameInput of nameInputs) {
+	new IMask(nameInput, {
+		mask: /^[а-яА-Я]*$/
+	})
+}
